51edddc247355256432e2dd91dd1d5756f68b64d
Boris Taiurskii <codeboris.spb@gmail.com>
2024-12-02 21:22:36 +0300

    add gin

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/codeboris/music-lib/pkg/server"
+	"github.com/sirupsen/logrus"
+)
 
 func main() {
-	fmt.Println("Hello World!!!")
+	logrus.SetFormatter(new(logrus.JSONFormatter))
+	logrus.Infoln("App logrus view.")
+
+	srv := new(server.Server)
+	srv.Run("8000", http.HandlerFunc(startString))
+}
+
+func startString(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Добро пожаловать на апи!"))
 }
